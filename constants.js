de040299dba2f7d3374f8931282321ecfcb4ba6b
ezshen <ezshen@stanford.edu>
2021-11-22 10:43:24 -0500

    cleanup

--- constants.js
+++ constants.js
@@ -1,14 +1,14 @@
 // Client ID of the cognito app, NOT your Sora client ID. This can be found after clicking "app clients" in the sidebar on the user pool page.
-export const client_id = "jscgcjrt67cn2ejib3rkfhf3d";
+export const client_id = "<YOUR_CLIENT_ID>";
 
 // Client secret of the cognito app, NOT your Sora client secret. This can be found after clicking "app clients" in the sidebar on the user pool page.
 //
 // Some apps don't use a client secret, in which case there is no authorization header
 // in fetch request in app.js
-export const client_secret = "oav5vbuko0r3e3s42uekbv0dhqepp5rpai6483da1rti7r844es";
+export const client_secret = "<YOUR_CLIENT_SECRET>";
 
 // base url of your cognito auth page. This can be found after clicking "domain name" (under "App integration") in the sidebar on the user pool page.
-export const auth_base_url = "https://sora-test-2.auth.us-east-2.amazoncognito.com";
+export const auth_base_url = "<YOUR_BASE_URL>";
 
 // redirect url for signing in. Replace the base url if not running on localhost.
-export const redirect_uri = "http://localhost:3000/signin"
+export const redirect_uri = "https://ethan.soraid.com/signin"
